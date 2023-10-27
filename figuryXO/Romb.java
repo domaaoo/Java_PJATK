251f0ed2e34df4c48a275f54d9d3adc55b72b079
domaaoo <83286526+domaaoo@users.noreply.github.com>
2023-10-27 13:33:11 +0200

    Update Romb.java

--- figuryXO/Romb.java
+++ figuryXO/Romb.java
@@ -1,7 +1,7 @@
 public class Romb {
     public static void main(String args[]) {
         
-        int n = 3;
+        int n = 3;//wysokosc diamentu
     
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
